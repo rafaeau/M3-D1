6dba30718501d0601cc64cbcd1214c68bfac1728
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-11-29 16:55:05 +0100

    Exercise 5

--- M3-D1.js
+++ M3-D1.js
@@ -42,6 +42,15 @@ console.log(returnLargest(15, 68, 20))
 /* 5. Create a function to check if two numbers are in the range 40-60 or 70-100. 
     Return `true` if they do, return `false` if one (or both) don't. */
 
+const checkRange = function (num1, num2) {
+    if (((num1 >= 40) && (num1 <= 60) || (num1 >= 70) && (num1 <= 100)) && ((num2 >= 40) && (num2 <= 60) || (num2 >= 70) && (num2 <= 100))) {
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(checkRange(43, 75))
 
 /* 6. Create a function to create a new string composed of a specified number of copies of a given string. 
     Pass the string and the number of copies as parameters. */
